961b8e04dcd6ec1d9b8caaead6f00a698af5f2a5
James Dorrington <jdorrington@hotmail.com>
2021-11-26 23:01:48 +0000

    Added outputting of scores and fixed clearing of screen. 'Just' ball movement and losing to complete

--- src/pong.asm
+++ src/pong.asm
@@ -16,17 +16,20 @@ includelib c:\\masm32\\m32lib\\masm32.lib
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .data
-STD_OUTPUT_HANDLE           equ -11                             ; https://docs.microsoft.com/en-us/windows/console/getstdhandle
+STD_OUTPUT_HANDLE           equ -11                                 ; https://docs.microsoft.com/en-us/windows/console/getstdhandle
         
-test_message                db "Hello world!", 13, 10           ; Our input/output byte
-TEST_MESSAGE_LEN            equ $ - offset test_message         ; Length of message
+test_message                db "Hello world!", 13, 10               ; Our input/output byte
+TEST_MESSAGE_LEN            equ $ - offset test_message             ; Length of message
+
+NUMBER_BUFFER_SIZE          equ 10                                  ; TODO: How big? How many digits?
+number_buffer               db NUMBER_BUFFER_SIZE dup(0)
 
 cursor_info                 CONSOLE_CURSOR_INFO <20, 0>
 console_buffer_info         CONSOLE_SCREEN_BUFFER_INFO <>
 
 SPACE                       db 32
 SPACE_LEN                   equ $ - offset SPACE
-BLOCK                       db 219                              ; Block char
+BLOCK                       db 219                                  ; Block char
 BLOCK_LEN                   equ $ - offset BLOCK
 BALL                        db 'O'
 BALL_LEN                    equ $ - offset BALL
@@ -39,6 +42,9 @@ PLAYER_1_DOWN_KEY           equ 'A'
 PLAYER_2_UP_KEY             equ 'P'
 PLAYER_2_DOWN_KEY           equ 'L'
 ESCAPE                      equ 27
+
+player_1_score              dw 0
+player_2_score              dw 0
 
 player_1_y                  dw 0
 player_1_x                  dw 0
@@ -62,44 +68,43 @@ TICK_INTERVAL               dd 10
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .data?
-console_out_handle          dd ?                                ; Our ouput handle (currently undefined)
-bytes_written               dd ?                                ; Number of bytes written to output (currently undefined)
+console_out_handle          dd ?                                    ; Our ouput handle (currently undefined)
+bytes_written               dd ?                                    ; Number of bytes written to output (currently undefined)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; .code section
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .code
-start:                      call get_output_handle              ; Get the input/output handles
-                            call hide_cursor
-                            call get_console_settings
+start:                      call get_output_handle                  ; Get the input/output handles
+                            call hide_cursor                        ; Hide the cursor
+                            call get_console_settings               ; Get the console width and height
+                            call clear_screen                       ; Clear the screen
                             
                             call GetTickCount                            
                             mov dword ptr [last_clock_ticks], eax
+                                                        
+                            mov ax, COLS_NEW                        ; Get the number of columns..
+                            dec ax                                  ; ..decrease by 1..
+                            mov word ptr [player_2_x], ax           ; ..and set the second player x position
                             
-                            mov ax, 0
-                            mov bx, 0
-                            call set_cursor_position
+                            mov ax, ROWS_NEW                        ; Get the number of rows..
+                            shr ax, 1                               ; ..bit-shift-right halves the value..
+                            mov word ptr [player_1_y], ax           ; ..and use the mid-point to set player 1 y position..
+                            mov word ptr [player_2_y], ax           ; ..and the same for player 2..
+                            mov word ptr [ball_y], ax               ; ..and for the ball
                             
-                            mov ax, COLS_NEW
-                            dec ax
-                            mov word ptr [player_2_x], ax
-                            
-                            mov ax, ROWS_NEW
-                            shr ax, 1
-                            mov word ptr [player_1_y], ax
-                            mov word ptr [player_2_y], ax
-                            mov word ptr [ball_y], ax
-                            
-                            mov ax, COLS_NEW
-                            shr ax, 1
-                            mov word ptr [ball_x], ax
+                            mov ax, COLS_NEW                        ; Get the number of columns again..
+                            shr ax, 1                               ; ..halve it so we are mid-screen..
+                            mov word ptr [ball_x], ax               ; ..and use it to set the ball x position
                             
                             
                             
                             
                             call draw_player_1
                             call draw_player_2
+                            call write_player_1_score
+                            call write_player_2_score
                             call draw_ball
                             
 
@@ -118,20 +123,7 @@ game_loop:                  push 20
                                                        
                             
                             ; Overwrite the current ball with a space
-                            mov eax, 0
-                            mov ebx, 0
-                            mov al, byte ptr [ball_y]
-                            mov bl, byte ptr [ball_x]
-                            
-                            push eax
-                            push ebx
-                            call set_cursor_position
-                            pop ebx
-                            pop eax
-
-                            push SPACE_LEN
-                            push offset SPACE
-                            call output_string
+                            call clear_ball
                             
                             ; calculate new ball position
                             
@@ -157,7 +149,7 @@ read_key:                   push ESCAPE
                             cmp ax, 0
                             jne end_program
                             
-                            push PLAYER_1_UP_KEY
+read_key_cont0:             push PLAYER_1_UP_KEY
                             call GetAsyncKeyState
                             cmp ax, 0
                             je read_key_cont1
@@ -182,9 +174,41 @@ read_key_cont3:             push PLAYER_2_DOWN_KEY
                             call player_2_down_pressed
                             
 read_key_cont4:
-
-
                             jmp game_loop
+
+player_1_lost:              inc word ptr [player_2_score]
+                            call write_player_2_score
+                            jmp player_lost
+
+player_2_lost:              inc word ptr [player_1_score]
+                            call write_player_1_score
+                            
+player_lost:                call clear_ball
+                            
+                            mov ax, ROWS_NEW                        ; Get the number of rows..
+                            shr ax, 1                               ; ..bit-shift-right halves the value..
+                            mov word ptr [ball_y], ax               ; ..and use it to set the ball y position
+                            
+                            mov ax, COLS_NEW                        ; Get the number of columns again..
+                            shr ax, 1                               ; ..halve it so we are mid-screen..
+                            mov word ptr [ball_x], ax               ; ..and use it to set the ball x position
+                           
+                            call draw_ball
+                            
+                            jmp game_loop
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; end_program
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+end_program:                call clear_screen
+                            call show_cursor
+                            push 0                          ; Exit code zero for success
+                            call ExitProcess                ; https://docs.microsoft.com/en-us/windows/desktop/api/processthreadsapi/nf-processthreadsapi-exitprocess
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; FUNCTIONS - FUNCTIONS - FUNCTIONS - FUNCTIONS - FUNCTIONS - FUNCTIONS - FUNCTIONS - FUNCTIONS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; player_1_up_pressed()
@@ -297,13 +321,39 @@ player_2_down_pressed:      mov ax, word ptr [player_2_y]
 player_2_down_pressed_done: ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; end_program
+; write_player_1_score()
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-end_program:                
-                            call show_cursor
-                            push 0                          ; Exit code zero for success
-                            call ExitProcess                ; https://docs.microsoft.com/en-us/windows/desktop/api/processthreadsapi/nf-processthreadsapi-exitprocess
+write_player_1_score:       mov ax, word ptr [ROWS_NEW]
+                            dec ax
+                            mov bx, word ptr [COLS_NEW]
+                            shr bx, 2
+                            call set_cursor_position
+                            
+                            push word ptr [player_1_score]
+                            call output_unsigned_byte
+                            
+                            ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; write_player_2_score()
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+write_player_2_score:       mov ax, word ptr [COLS_NEW]
+                            shr ax, 2
+                            mov bx, 3
+                            mul bx
+                            mov bx, ax
+                            
+                            mov ax, word ptr [ROWS_NEW]
+                            dec ax
+                            
+                            call set_cursor_position
+                            
+                            push word ptr [player_2_score]
+                            call output_unsigned_byte
+                            
+                            ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; draw_player_1()
@@ -362,6 +412,27 @@ draw_player_2_write_char:   push eax
                             ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; clear_ball()
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+clear_ball:                 mov eax, 0
+                            mov ebx, 0
+                            mov al, byte ptr [ball_y]
+                            mov bl, byte ptr [ball_x]
+                            
+                            push eax
+                            push ebx
+                            call set_cursor_position
+                            pop ebx
+                            pop eax
+
+                            push SPACE_LEN
+                            push offset SPACE
+                            call output_string
+                            
+                            ret
+                            
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; draw_ball()
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -402,6 +473,7 @@ get_console_settings:       lea eax, [console_buffer_info]
                             sub ax, bx
                             inc ax
                             mov word ptr [ROWS_NEW], ax 
+                            
                             ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -414,6 +486,7 @@ hide_cursor:                mov dword ptr [cursor_info.dwSize], 20
                             push eax
                             push console_out_handle
                             call SetConsoleCursorInfo
+                            
                             ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -426,6 +499,7 @@ show_cursor:                mov dword ptr [cursor_info.dwSize], 20
                             push eax
                             push console_out_handle
                             call SetConsoleCursorInfo
+                            
                             ret
                             
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -456,6 +530,78 @@ output_string:              pop ebp                         ; Pop the return add
 
                             push ebp                        ; Restore return address
                             ret                             ; Return to caller
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; output_unsigned_byte(BYTE: number)
+; Destroys: EBP, 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+output_unsigned_byte:       pop ebp                         ; Pop the return address
+                            pop ax                          ; Pop integer to output into AX
+                            push ebp                        ; Push EBP back onto stack
+
+                            and ax, 00FFh                   ; Make sure AX is in range 0..255                    
+                            mov ecx, 0                      ; Set digits counter to zero     
+                    
+output_unsigned_byte_perform_calculation:                    
+                            mov dx, 0
+                            mov bx, 10                      ; Divide by 10
+                            div bx                          ; Divide AX by BX                    
+                                                            ; DL contains remainer, AL contains quotient
+                            and edx, 000000FFh              ; Make sure EDX (remainer) is in range 0..255
+                            add dl, 030h                    ; Add 30h (the letter '0' (zero)) so we map numbers to letters
+                            push dx                         ; Push our letter to the stack
+                            inc ecx                         ; Increment digit counter
+
+                            cmp al, 0                       ; Check if quotient is zero
+                            jne output_unsigned_byte_perform_calculation    ; If quotient is not zero, then we need to perform the operation again
+
+                            mov edi, 0                      ; Set EDI to zero. This will point to 'number_buffer' starting at index 0
+output_unsigned_byte_finished_calculation:
+                            pop dx                          ; Read the last remainder from the stack
+ 
+                            mov byte ptr [number_buffer + edi], dl ; Copy the letter to 'number_buffer'
+                    
+                            inc edi                         ; Incrememnt out pointer to 'number_buffer'
+                            loop output_unsigned_byte_finished_calculation  ; Continue looping until ECX is zero
+
+                            push edi                        ; At the end of the process, EDI will conveniently hold the number of characters written to 'number_buffer'. Pass it as a parameter to 'output_string'
+                            push offset number_buffer
+                            call output_string      
+  
+                            ret                             ; Return to caller
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; clear_screen()
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;al = y bl = x
+clear_screen:               mov eax, 0
+                            mov ebx, 0
+                            
+clear_screen_col_loop:      push eax
+                            push ebx
+                            call set_cursor_position
+                            pop ebx
+                            pop eax
+
+                            push eax
+                            push ebx
+                            push SPACE_LEN
+                            push offset SPACE
+                            call output_string 
+                            pop ebx
+                            pop eax
+                            
+                            inc ebx
+                            cmp bx, word ptr [COLS_NEW]
+                            jne clear_screen_col_loop
+                            
+                            mov ebx, 0
+                            inc eax
+                            cmp ax, word ptr[ROWS_NEW]
+                            jne clear_screen_col_loop
+                            
+                            ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; get_output_handle()

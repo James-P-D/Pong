05a7f1d83b2c5ee067ef01f0dbdc0454d3bbde44
James Dorrington <jdorrington@hotmail.com>
2021-11-26 21:21:51 +0000

    Bunch of changes cleaning up screen resolution

--- src/pong.asm
+++ src/pong.asm
@@ -11,19 +11,28 @@ includelib c:\\masm32\\lib\\kernel32.lib
 includelib c:\\masm32\\lib\\user32.lib 
 includelib c:\\masm32\\m32lib\\masm32.lib
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; .data section
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 .data
 STD_OUTPUT_HANDLE           equ -11                             ; https://docs.microsoft.com/en-us/windows/console/getstdhandle
-
         
 test_message                db "Hello world!", 13, 10           ; Our input/output byte
 TEST_MESSAGE_LEN            equ $ - offset test_message         ; Length of message
+
+cursor_info                 CONSOLE_CURSOR_INFO <20, 0>
+console_buffer_info         CONSOLE_SCREEN_BUFFER_INFO <>
+
 SPACE                       db 32
 SPACE_LEN                   equ $ - offset SPACE
 BLOCK                       db 219                              ; Block char
 BLOCK_LEN                   equ $ - offset BLOCK
+BALL                        db 'O'
+BALL_LEN                    equ $ - offset BALL
 
-COLS                        equ 80
-ROWS                        equ 29
+COLS_NEW                    dw 0
+ROWS_NEW                    dw 0
 
 PLAYER_1_UP_KEY             equ 'Q'
 PLAYER_1_DOWN_KEY           equ 'A'
@@ -31,32 +40,119 @@ PLAYER_2_UP_KEY             equ 'P'
 PLAYER_2_DOWN_KEY           equ 'L'
 ESCAPE                      equ 27
 
-player_1_y                  db (ROWS / 2) - (PADDLE_SIZE / 2)
-player_1_x                  equ 1
-player_2_y                  db (ROWS / 2) - (PADDLE_SIZE / 2)
-player_2_x                  equ COLS - 2
+player_1_y                  dw 0
+player_1_x                  dw 0
+player_2_y                  dw 0
+player_2_x                  dw 0
 PADDLE_SIZE                 equ 5
+
+BALL_DIR_SE                 equ 0                                   ; South east movement
+BALL_DIR_SW                 equ 1                                   ; South west movement
+BALL_DIR_NW                 equ 2                                   ; North west movement
+BALL_DIR_NE                 equ 3                                   ; North east movement
+ball_dir                    db BALL_DIR_SE
+ball_x                      dw 0
+ball_y                      dw 0
+
+last_clock_ticks            dw 0
+TICK_INTERVAL               dd 10
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; .data? section
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .data?
 console_out_handle          dd ?                                ; Our ouput handle (currently undefined)
 bytes_written               dd ?                                ; Number of bytes written to output (currently undefined)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; .code section
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 .code
 start:                      call get_output_handle              ; Get the input/output handles
-                            ;call hide_cursor
+                            call hide_cursor
+                            call get_console_settings
+                            
+                            call GetTickCount                            
+                            mov dword ptr [last_clock_ticks], eax
                             
                             mov ax, 0
                             mov bx, 0
                             call set_cursor_position
                             
+                            mov ax, COLS_NEW
+                            dec ax
+                            mov word ptr [player_2_x], ax
+                            
+                            mov ax, ROWS_NEW
+                            shr ax, 1
+                            mov word ptr [player_1_y], ax
+                            mov word ptr [player_2_y], ax
+                            mov word ptr [ball_y], ax
+                            
+                            mov ax, COLS_NEW
+                            shr ax, 1
+                            mov word ptr [ball_x], ax
+                            
+                            
+                            
+                            
                             call draw_player_1
                             call draw_player_2
+                            call draw_ball
                             
 
-read_key:                   push 50
+game_loop:                  push 20
                             call Sleep
                             
-                            push ESCAPE
+                            ;call GetTickCount
+                            ;
+                            ;mov ebx, dword ptr [last_clock_ticks]
+                            ;add ebx, TICK_INTERVAL
+                            ;  
+                            ;cmp eax, ebx
+                            ;jge read_key
+                            ;
+                            ;mov dword ptr [last_clock_ticks], eax
+                                                       
+                            
+                            ; Overwrite the current ball with a space
+                            mov eax, 0
+                            mov ebx, 0
+                            mov al, byte ptr [ball_y]
+                            mov bl, byte ptr [ball_x]
+                            
+                            push eax
+                            push ebx
+                            call set_cursor_position
+                            pop ebx
+                            pop eax
+
+                            push SPACE_LEN
+                            push offset SPACE
+                            call output_string
+                            
+                            ; calculate new ball position
+                            
+                            ;cmp byte ptr [ball_dir], BALL_DIR_SE
+                            ;jne check_ball_dir_sw
+                            
+                            ;inc byte ptr [ball_x]
+                            ;inc byte ptr [ball_y]
+                            ;jmp ??????
+                            
+check_ball_dir_sw:                            
+                            
+check_ball:                            
+                            ; check if wall
+                            ; check if batt
+                        
+                            call draw_ball
+                            
+                            
+                            
+read_key:                   push ESCAPE
                             call GetAsyncKeyState
                             cmp ax, 0
                             jne end_program
@@ -88,7 +184,7 @@ read_key_cont3:             push PLAYER_2_DOWN_KEY
 read_key_cont4:
 
 
-                            jmp read_key
+                            jmp game_loop
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; player_1_up_pressed()
@@ -120,18 +216,21 @@ player_1_up_pressed_done:   ret
 ; player_1_down_pressed()
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-player_1_down_pressed:      mov al, byte ptr [player_1_y]
-                            cmp al, ROWS - PADDLE_SIZE
+player_1_down_pressed:      mov ax, word ptr [player_1_y]
+                            mov bx, ROWS_NEW
+                            sub bx, PADDLE_SIZE
+                            dec bx                            
+                            cmp ax, bx
                             je player_1_down_pressed_done
                             
-                            inc al
-                            mov byte ptr [player_1_y], al
+                            inc ax
+                            mov word ptr [player_1_y], ax
                             
                             mov eax, 0
-                            mov al, byte ptr [player_1_y]
-                            dec al
+                            mov ax, word ptr [player_1_y]
+                            dec ax
                             mov ebx, 0
-                            mov bl, byte ptr [player_1_x]
+                            mov bx, word ptr [player_1_x]
                             call set_cursor_position
                             
                             push SPACE_LEN
@@ -146,18 +245,18 @@ player_1_down_pressed_done: ret
 ; player_2_up_pressed()
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-player_2_up_pressed:        mov al, byte ptr [player_2_y]
-                            cmp al, 0
+player_2_up_pressed:        mov ax, word ptr [player_2_y]
+                            cmp ax, 0
                             je player_2_up_pressed_done
 
-                            dec al                            
-                            mov byte ptr [player_2_y], al
+                            dec ax                            
+                            mov word ptr [player_2_y], ax
                             
                             mov eax, 0
-                            mov al, byte ptr [player_2_y]
+                            mov ax, word ptr [player_2_y]
                             add eax, PADDLE_SIZE
                             mov ebx, 0
-                            mov bl, byte ptr [player_2_x]
+                            mov bx, word ptr [player_2_x]
                             call set_cursor_position
                             
                             push SPACE_LEN
@@ -172,18 +271,21 @@ player_2_up_pressed_done:   ret
 ; player_2_down_pressed()
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-player_2_down_pressed:      mov al, byte ptr [player_2_y]
-                            cmp al, ROWS - PADDLE_SIZE
+player_2_down_pressed:      mov ax, word ptr [player_2_y]
+                            mov bx, ROWS_NEW
+                            sub bx, PADDLE_SIZE
+                            dec bx
+                            cmp ax, bx
                             je player_2_down_pressed_done
                             
-                            inc al
-                            mov byte ptr [player_2_y], al
+                            inc ax
+                            mov word ptr [player_2_y], ax
                             
                             mov eax, 0
-                            mov al, byte ptr [player_2_y]
-                            dec al
+                            mov ax, word ptr [player_2_y]
+                            dec ax
                             mov ebx, 0
-                            mov bl, byte ptr [player_2_x]
+                            mov bx, word ptr [player_2_x]
                             call set_cursor_position
                             
                             push SPACE_LEN
@@ -198,9 +300,8 @@ player_2_down_pressed_done: ret
 ; end_program
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
 end_program:                
-                            ;call show_cursor
+                            call show_cursor
                             push 0                          ; Exit code zero for success
                             call ExitProcess                ; https://docs.microsoft.com/en-us/windows/desktop/api/processthreadsapi/nf-processthreadsapi-exitprocess
 
@@ -210,8 +311,8 @@ end_program:
 
 draw_player_1:              mov eax, 0
                             mov ebx, 0
-                            mov al, byte ptr [player_1_y]
-                            mov bl, byte ptr [player_1_x]
+                            mov ax, word ptr [player_1_y]
+                            mov bx, word ptr [player_1_x]
                             mov ecx, PADDLE_SIZE
                             
 draw_player_1_write_char:   push eax
@@ -238,8 +339,8 @@ draw_player_1_write_char:   push eax
 
 draw_player_2:              mov eax, 0
                             mov ebx, 0
-                            mov al, byte ptr [player_2_y]
-                            mov bl, byte ptr [player_2_x]
+                            mov ax, word ptr [player_2_y]
+                            mov bx, word ptr [player_2_x]
                             mov ecx, PADDLE_SIZE
                             
 draw_player_2_write_char:   push eax
@@ -261,27 +362,71 @@ draw_player_2_write_char:   push eax
                             ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; draw_ball()
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+draw_ball:                  mov eax, 0
+                            mov ebx, 0
+                            mov ax, word ptr [ball_y]
+                            mov bx, word ptr [ball_x]
+                            
+                            push eax
+                            push ebx
+                            call set_cursor_position
+                            pop ebx
+                            pop eax
+
+                            push BALL_LEN
+                            push offset BALL
+                            call output_string
+                            
+                            ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; get_console_settings()
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+get_console_settings:       lea eax, [console_buffer_info]
+                            push eax
+                            push console_out_handle                            
+                            call GetConsoleScreenBufferInfo
+                            
+                            mov ax, word ptr [console_buffer_info.srWindow.Right]
+                            mov bx, word ptr [console_buffer_info.srWindow.Left]
+                            sub ax, bx
+                            inc ax
+                            mov word ptr [COLS_NEW], ax 
+                            
+                            mov ax, word ptr [console_buffer_info.srWindow.Bottom]
+                            mov bx, word ptr [console_buffer_info.srWindow.Top]
+                            sub ax, bx
+                            inc ax
+                            mov word ptr [ROWS_NEW], ax 
+                            ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; hide_cursor()
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;hide_cursor:                mov dword ptr [CONSOLE_CURSOR_INFO.dwSize], 50
-;                            mov dword ptr [CONSOLE_CURSOR_INFO.bVisible], 0
-;                            lea eax, [CONSOLE_CURSOR_INFO_size]
-;                            push eax
-;                            push console_out_handle
-;                            call SetConsoleCursorInfo
-;                            ret
+hide_cursor:                mov dword ptr [cursor_info.dwSize], 20
+                            mov dword ptr [cursor_info.bVisible], 0
+                            lea eax, [cursor_info]
+                            push eax
+                            push console_out_handle
+                            call SetConsoleCursorInfo
+                            ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; show_cursor()
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;show_cursor:                ;mov byte ptr [CONSOLE_CURSOR_INFO_visible], 0
-;                            lea eax, [CONSOLE_CURSOR_INFO_size]
-;                            push eax
-;                            push console_out_handle
-;                            call SetConsoleCursorInfo
-;                            ret
+show_cursor:                mov dword ptr [cursor_info.dwSize], 20
+                            mov dword ptr [cursor_info.bVisible], 1
+                            lea eax, [cursor_info]
+                            push eax
+                            push console_out_handle
+                            call SetConsoleCursorInfo
+                            ret
                             
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; set_cursor_position(ax = y, bx = x)
